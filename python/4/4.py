b51ca6600c7d51b916fe1ce3a7d6479038c7e130
Sean McGlothlin <sean.michael.mcglothlin@gmail.com>
2020-12-04 16:05:34 -0800

    part 2, not quite working

--- python/4/4.py
+++ python/4/4.py
@@ -1,7 +1,8 @@
 import re
 from dataclasses import dataclass
+from enum import Enum
 from typing import Optional, List
-from common import readString
+from common import readString, inclusiveRange
 
 
 @dataclass
@@ -16,7 +17,7 @@ class Passport:
     height: str
     hair_color: str
     eye_color: str
-    passport_id: int
+    passport_id: str
     country_id: Optional[int]
 
 
@@ -36,13 +37,62 @@ class PassportAdapter(Passport):
         self.passport_id = pid
         self.country_id = cid
 
+    def validate_inputs(self):
+        self.validate_range(self.birth_year, 1920, 2002)
+        self.validate_range(self.issue_year, 2010, 2020)
+        self.validate_range(self.expiration_year, 2020, 2030)
+        self.validate_height(self.height)
+        self.validate_pattern(self.hair_color, re.compile(r'#[0-9a-f]{6}'))
+        self.validate_eyes(self.eye_color)
+        self.validate_pattern(self.passport_id, re.compile(r'[0-9]{9}'))
+
+    @staticmethod
+    def validate_range(number, start, end):
+        try:
+            if int(number) not in inclusiveRange(start, end):
+                raise TypeError(f'Invalid number "{number}". Must be between {start} and {end}.')
+        except ValueError:
+            raise TypeError(f'Invalid number. {number} is not an integer.')
+
+    @staticmethod
+    def validate_pattern(input_str, pattern):
+        """ Validates a generic pattern """
+        if not pattern.match(input_str):
+            raise TypeError(f'Invalid passport. {input_str} must match: {pattern}.')
+
+    def validate_height(self, height):
+        """ Height range based on units selected. """
+        valid_units = ['cm', 'in']
+        parsed_height = re.split(r'(\d{2}\d?)(\w{2})', height)
+        try:
+            assert len(parsed_height) == 4
+        except AssertionError:
+            raise TypeError(f'Invalid height. Expecting a number and a unit of measurement.')
+        distance_value = parsed_height[1]
+        distance_unit  = parsed_height[2]
+
+        if distance_unit not in valid_units:
+            raise TypeError(f'Invalid height. Must measure in cm or in.')
+
+        if distance_unit == 'in':
+            self.validate_range(distance_value, 59, 76)
+        if distance_unit == 'cm':
+            self.validate_range(distance_value, 150, 193)
+
+    @staticmethod
+    def validate_eyes(eye_color):
+        valid_eye_colors = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        if eye_color not in valid_eye_colors:
+            raise TypeError(f'Invalid eye color {eye_color}. Must be one of {", ".join(valid_eye_colors)}')
+
 
 class PassportParser:
-    def __init__(self):
+    def __init__(self, validate=True):
         """ separator indicates a break between passport entries """
         self.valid_passports: List[Passport] = []
         self.invalid_passports: List[dict] = []
-
+        self.expected_fields = 7
+        self.validate = validate
 
     def parse_file(self, file_path):
         passport_separator = re.compile('\n{2}')  # Separates distinct passports
@@ -62,25 +112,34 @@ class PassportParser:
                 key, value = re.split(key_val_separator, field)
                 passport_dict[key] = value
             try:
-                self.valid_passports.append(PassportAdapter(**passport_dict))
-            except TypeError:
-                # print(f'Invalid passport spec: {passport_dict}')
+                passport = PassportAdapter(**passport_dict)
+                if self.validate:  # Part 2
+                    passport.validate_inputs()
+                self.valid_passports.append(passport)
+            except TypeError as e:
+                # This try/catch should be used with care. Could easily swallow a legitimate exception, making the
+                # validation harder to debug.
                 self.invalid_passports.append(passport_dict)
-
+                # print(e)
 
     def get_passports(self):
         return self.valid_passports
 
 
 def main():
-    parser = PassportParser()
-    parser.parse_file('input.txt')
+    parser1 = PassportParser(validate=False)
+    parser1.parse_file('input.txt')
 
-    part1 = len(parser.get_passports())
-    # part2 = treesHit(1, 1) * treesHit(3, 1) * treesHit(5, 1) * treesHit(7, 1) * treesHit(1, 2)
+    parser2 = PassportParser(validate=True)
+    parser2.parse_file('input.txt')
+
+    part1 = len(parser1.get_passports())
+    part2 = len(parser2.get_passports())
+    foo = parser2.get_passports()
+    print(foo)
 
     print(f'Answer 1: {part1}')
-    # print(f'Answer 2: {part2}')
+    print(f'Answer 2: {part2}')
 
 
 if __name__ == '__main__':
